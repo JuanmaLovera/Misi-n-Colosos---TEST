442dad3cede6b3deddfadf7afc1e73171010667f
JuanmaLovera <atodopulmongis04@gmail.com>
2026-07-22 22:41:01 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE="mision-colosos-v1";
+
+const FILES=[
+ "./",
+ "./index.html",
+ "./style.css",
+ "./app.js",
+ "./survey-logic.js",
+ "./coti.png",
+ "./icon-192.png",
+ "./icon-512.png"
+];
+
+self.addEventListener("install",e=>{
+ e.waitUntil(
+   caches.open(CACHE).then(c=>c.addAll(FILES))
+ );
+ self.skipWaiting();
+});
+
+self.addEventListener("activate",e=>{
+ e.waitUntil(
+   caches.keys().then(keys=>
+     Promise.all(
+       keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+     )
+   )
+ );
+ self.clients.claim();
+});
+
+self.addEventListener("fetch",e=>{
+ if(e.request.method!=="GET") return;
+
+ e.respondWith(
+   fetch(e.request)
+   .then(r=>{
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return r;
+   })
+   .catch(()=>caches.match(e.request))
+ );
+});
